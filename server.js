6a779965524caead70c436bbc41e0a9cc44508c5
ted sandridge <sand0959@gmail.com>
2017-07-30 16:32:38 -0400

    routes working somewhat

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 
-"use strict"
+import mongoose from 'mongoose';
 import express from "express";
 import bodyParser from "body-parser";
-import spotify from "./routes/spotifyRoutes";
+// import spotify from "./routes/spotifyRoutes";
 import Oauth from "./routes/Oauth";
 import path from "path";
-
+import Mongo from './routes/mongoRoutes';
 var app = express();
 
 const PORT = process.env.PORT || 8080;
+
 
 app.use(express.static(path.join(__dirname, "node_modules")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //Add music api here
-app.use("/spotify",spotify);
+// app.use("/spotify",spotify);
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
 app.use('/auth',Oauth);
+app.use('/db', Mongo);
+});
+
+mongoose.connect('mongodb://localhost/queue');
+
 
 //This launches the server on port 4000
 
